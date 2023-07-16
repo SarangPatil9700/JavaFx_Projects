b60680e7472307285f872bdad5ff4aaad3bea4e3
Sarangp9700 <sarangpatil9700@gmail.com>
2023-07-16 11:12:51 +0530

    push

---
+++ Daily Codes/Exception Handling/Arithmetic Exception(DEH)/arithex.java
@@ -0,0 +1,14 @@
+class Demo{
+	void m2(){
+		System.out.println("In m2");
+	}
+void m1(){
+	System.out.println(10/0);
+	m2();
+}
+public static void main(String[]args){
+	Demo obj=new Demo();
+	obj.m1();
+	obj.m2();
+}
+}
