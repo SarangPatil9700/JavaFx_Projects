7d4872f7986f6de06ac25a752d1a6db225b34e58
Sarangp9700 <sarangpatil9700@gmail.com>
2023-07-16 10:41:57 +0530

    push

---
+++ Daily Codes/Inner class/program4.java
@@ -0,0 +1,18 @@
+class Outer{
+	object m1(){
+		System.out.println("In m1-outer");
+		class Inner{
+			void m1(){
+				System.out.println("In m1-outer");
+			}
+		}
+        	return new Inner();
+	}
+}
+class Client{
+	public static void main(String[]args){
+		Outer obj=new Outer();
+		obj.m1().m1();
+	}
+}
+
